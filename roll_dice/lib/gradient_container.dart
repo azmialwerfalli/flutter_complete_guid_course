17cf7502ada6c04818847f94d7305fcaed03a9d3
Azmi Alfoghi <drama.com.aa@gmail.com>
2023-12-18 17:25:05 +0200

    another usage of const

--- roll_dice/lib/gradient_container.dart
+++ roll_dice/lib/gradient_container.dart
@@ -5,24 +5,27 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1,this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer.purple({super.key})
+      : color1 = const Color.fromARGB(255, 26, 2, 80),
+        color2 = const Color.fromARGB(255, 106, 0, 255);
   final Color color1;
   final Color color2;
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [color1,color2],
-              begin: startAlignment,
-              end: endAlignment,
-            ),
-          ),
-          child:Center(
-            child: StyledText('hellos'),
-          ),
-        );
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Image.asset('dice-1.png'),
+      ),
+    );
   }
 }
 // class GradientContainer extends StatelessWidget {
