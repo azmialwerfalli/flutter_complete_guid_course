cf2da7605fac25eeb06bb66cf7e28e7500f713e1
Azmi Alfoghi <drama.com.aa@gmail.com>
2023-12-18 16:42:45 +0200

    const and final

--- roll_dice/lib/gradient_container.dart
+++ roll_dice/lib/gradient_container.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context){
     return Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: const[
+              colors: [
                 Color.fromARGB(255, 26, 2, 80),
                 Color.fromARGB(255, 106, 0, 255),
               ],
