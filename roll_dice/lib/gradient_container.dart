925b905092345ebfa450a0d35430ba911bfa4c83
Azmi Alfoghi <drama.com.aa@gmail.com>
2023-12-18 17:31:09 +0200

    functions

--- roll_dice/lib/gradient_container.dart
+++ roll_dice/lib/gradient_container.dart
@@ -5,27 +5,30 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
-  const GradientContainer.purple({super.key})
-      : color1 = const Color.fromARGB(255, 26, 2, 80),
-        color2 = const Color.fromARGB(255, 106, 0, 255);
+  const GradientContainer(this.color1,this.color2, {super.key});
+  // const GradientContainer.purple({super.key}) : color1 =
   final Color color1;
   final Color color2;
 
   @override
-  Widget build(context) {
+  Widget build(context){
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color1, color2],
-          begin: startAlignment,
-          end: endAlignment,
-        ),
-      ),
-      child: Center(
-        child: Image.asset('dice-1.png'),
-      ),
-    );
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color1,color2],
+              begin: startAlignment,
+              end: endAlignment,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset('assets/images/dice-5.png',width: 200,),
+                TextButton(onPressed: () {}, child: const Text('click me'))
+              ],
+            ),
+          ),
+        );
   }
 }
 // class GradientContainer extends StatelessWidget {
--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer.purple(),
+        body: GradientContainer(Color.fromARGB(255, 26, 2, 80),Color.fromARGB(255, 106, 0, 255),),
       ),
     ),
   );
