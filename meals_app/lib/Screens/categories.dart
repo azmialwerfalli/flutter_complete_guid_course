bde5acd32f4b0407e3b3b4c0b63f9a775968644a
Azmi Alfoghi <drama.com.aa@gmail.com>
2024-01-01 19:17:18 +0200

    ep10.

--- meals_app/lib/Screens/categories.dart
+++ meals_app/lib/Screens/categories.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/Screens/meals.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/Models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filterdMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.of(context).push(MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'title', meals: [])));
+        builder: (ctx) =>  MealsScreen(
+              title: category.title,
+              meals: filterdMeals,
+            )));
     // Navigator.push(context, route)
   }
 
@@ -31,7 +38,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: () => _selectCategory(context),
+              onSelectCategory: () => _selectCategory(context, category),
             )
         ],
       ),
